c347954535d5ed0395323ff46934bfe07598143f
htpham <htpham3096@gmail.com>
2021-09-15 08:58:49 +0700

    nestjs investigate project

---
+++ task-management/src/tasks/tasks.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { FilterTaskDto } from './dto/filter-task.dto';
+import { Task } from './dto/task.entity';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { TaskStatus } from './tasks-status.enum.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(
+    @Query() filterTaskDto: FilterTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    return this.tasksService.getAllTasks(filterTaskDto, user);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(
+    @Param('id') id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
+  }
+
+  @Post()
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto, user);
+  }
+
+  @Patch('/:id')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    const { status } = updateTaskDto;
+    return this.tasksService.updateTaskStatus(id, status, user);
+  }
+}
---
+++ task-management/src/tasks/tasks.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './dto/task.repository.dto';
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
+  providers: [TasksService],
+  controllers: [TasksController],
+})
+export class TasksModule {}
